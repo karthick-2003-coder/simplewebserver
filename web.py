9f58af9120fc7d7d01dfab468a89661021a96f82
KARTHICK <135232854+karthick-2003-coder@users.noreply.github.com>
2023-10-13 22:44:10 +0530

    Add files via upload

--- web.py
+++ web.py
@@ -1,26 +1,5 @@
-# EX01 Developing a Simple Webserver
-## Date:12.10.23
-
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Serving the HTML pages.
-
-### Step 5:
-Testing the webserver.
-
-## PROGRAM:
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
 <!DOCTYPE html>
 <html>
 <head>
@@ -49,11 +28,3 @@ server_address = ('',8000)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-
-## OUTPUT:
-![Alt text](<Screenshot (6).png>)
-
-
-
-## RESULT:
-The program for implementing simple webserver is executed successfully.
